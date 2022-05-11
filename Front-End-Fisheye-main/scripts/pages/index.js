3580c6762bca8632258f5ca408a77fd7ae04da2a
Erik <93394265+Erik-TH@users.noreply.github.com>
2022-05-11 13:30:24 +0200

    reset project

--- Front-End-Fisheye-main/scripts/pages/index.js
+++ Front-End-Fisheye-main/scripts/pages/index.js
@@ -1,46 +1,45 @@
-    /**
-     * 
-     * @returns {photographers} la map des photographes, données traitées 
-     */
-     async function getPhotographers() {
-        new BaseURL
-        const photographersApi = new PhotographersApi(BaseURL.base+"data/photographers.json")
-        const photographersData = await photographersApi.getPhotographers()
-        
-        const photographers = photographersData
-                                .map(
-                                    photographer => photographerFactory(photographer)
-                                    )
-        return ({photographers: photographers})
+    async function getPhotographers() {
+        // Penser à remplacer par les données récupérées dans le json
+        const photographers = [
+            {
+                "name": "Ma data test",
+                "id": 1,
+                "city": "Paris",
+                "country": "France",
+                "tagline": "Ceci est ma data test",
+                "price": 400,
+                "portrait": "account.png"
+            },
+            {
+                "name": "Autre data test",
+                "id": 2,
+                "city": "Londres",
+                "country": "UK",
+                "tagline": "Ceci est ma data test 2",
+                "price": 500,
+                "portrait": "account.png"
+            },
+        ]
+        // et bien retourner le tableau photographers seulement une fois
+        return ({
+            photographers: [...photographers, ...photographers, ...photographers]})
     }
 
-    /**
-     * 
-     * @param {*} photographers 
-     * La collection de photographes
-     */
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
 
         photographers.forEach((photographer) => {
-            const userCardDOM = photographer.getUserCardDOM();
+            const photographerModel = photographerFactory(photographer);
+            const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
         });
     };
 
-    /**
-     * Fonction d'initialisation.
-     * Affichage des vignettes et des évènements qui y sont liés
-     */
     async function init() {
-        // Récupère et traite les datas des photographes
+        // Récupère les datas des photographes
         const { photographers } = await getPhotographers();
-        // Et les affiche
         displayData(photographers);
     };
     
-    /**
-     * index.html : Première fonction appelée
-     */
     init();
     
